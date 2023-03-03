a119ad3443a6c25a2d4b724f193168bdbf5ae1f3
antonio moschetti <antoniomoschetti@hotmail.it>
2023-03-03 14:43:46 +0000

    created a TestEvent model and controller, created js controllers for stimulus, sortable, geocoder and cloudinary, created seed, added gems including faker, added a test_card.scss

---
+++ app/javascript/controllers/edit_event_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="edit-movie"
+export default class extends Controller {
+  static targets = ["infos", "form", "card","details","forms"]
+
+  displayDetails() {
+    // this.infosTarget.classList.add("d-none")
+    this.detailsTarget.classList.remove("d-none")
+  }
+  displayForm() {
+    console.log("displayForm")
+    // this.infosTarget.classList.add("d-none")
+    this.formTarget.classList.remove("d-none")
+
+    this.detailsTarget.classList.add("d-none")
+  }
+  hideDetails() {
+    // this.infosTarget.classList.remove("d-none")
+    this.detailsTarget.classList.add("d-none")
+  }
+  hideForm() {
+    // this.infosTarget.classList.remove("d-none")
+    this.formTarget.classList.add("d-none")
+  }
+
+  update(event) {
+    event.preventDefault()
+    const url = this.formTarget.action
+    fetch(url, {
+      method: "PATCH",
+      headers: { "Accept": "text/plain" },
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.text())
+      .then((data) => {
+        this.cardTarget.outerHTML = data
+      })
+  }
+}
