3a808f36bd728d92eba9519f3ccf5c308acb9726
antonio moschetti <antoniomoschetti@hotmail.it>
2023-03-06 10:25:03 +0000

    index.js fixed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import EditEventController from "./edit_event_controller"
 application.register("edit-event", EditEventController)
 
+import AddressController from "./address_controller"
+application.register("address", AddressController)
+
 import EventsController from "./events_controller"
 application.register("events", EventsController)
 
@@ -18,3 +21,14 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+
+import { initSortable } from "./init_sortable"
+initSortable()
+
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("./", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
