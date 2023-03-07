0e45c54e2f2a676c0497df25462cad38a759d248
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-03-07 13:34:10 +0000

    gets place and alternative places

--- app/services/populate_itinerary.rb
+++ app/services/populate_itinerary.rb
@@ -17,27 +17,38 @@ class PopulateItinerary < ApplicationRecord
   private
 
   def populate_itinerary
-    location = "#{@itinerary.latitude}, #{@itinerary.longitude}"
+    location = "#{@itinerary.latitude},#{@itinerary.longitude}"
 
-    event_places = []
+    @itinerary_template.each do |event_details|
 
-    @itinerary_template.each do |event|
+      event = Event.new
+      event.itinerary = @itinerary
+      event.start_time = event_details[:event_start_time]
+      event.end_time = event_details[:event_end_time]
+      event.order_number = event_details[:order_number]
+
       search_location = location
+      url = generate_url(search_location, event_details)
+      alternative_places = fetch_places(url, event_details) # DELETE EVENT ARG
+      place = alternative_places["results"].shift
 
-      url = generate_url(search_location, event)
-      places = fetch_places(url, event) # DELETE EVENT ARG
-      # event_places << places
+      place_details = populate_place(place)
+
+
+
+      raise
+
     end
-    raise
+
   end
 
-  def generate_url(search_location, event)
+  def generate_url(search_location, event_details)
     key = 1234
 
-    URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{search_location}&radius=#{event[:radius]}&type=#{event[:place_type]}&keyword=#{event[:keyword]}&maxprice=#{event[:maxprice]}&rankby=prominence&key=#{key}")
+    URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{search_location}&radius=#{event_details[:radius]}&type=#{event_details[:place_type]}&keyword=#{event_details[:keyword]}&maxprice=#{event_details[:maxprice]}&rankby=prominence&key=#{key}")
   end
 
-  def fetch_places(url, event) # DELETE EVENT PARAM
+  def fetch_places(url, event_details) # DELETE EVENT PARAM
     # https = Net::HTTP.new(url.host, url.port)
     # https.use_ssl = true
 
@@ -49,36 +60,48 @@ class PopulateItinerary < ApplicationRecord
     # JSON.parse(response_json)
 
     # TEST JSONS
-    if event[:input_category] == "History"
+    if event_details[:input_category] == "History"
       filepath = "app/services/test_results/museum_history|culture|immersive.json"
-    elsif event[:input_category] == "Art & Culture"
+    elsif event_details[:input_category] == "Art & Culture"
       filepath = "app/services/test_results/art_gallery_art|culture|exhibition.json"
-    elsif event[:input_category] == "Shopping"
+    elsif event_details[:input_category] == "Shopping"
       filepath = "app/services/test_results/establishment_ shopping|mall|centre|department|store.json"
-    elsif event[:input_category] == "dining_dinner"
+    elsif event_details[:input_category] == "dining_dinner"
       filepath = "app/services/test_results/restaurant_japanese|ramen_minprice-_maxprice-.json"
-    elsif event[:input_category] == "dining_lunch"
+    elsif event_details[:input_category] == "dining_lunch"
       filepath = "app/services/test_results/restaurant_japanese|ramen_minprice-_maxprice-.json"
-    elsif event[:input_category] == "Drinks"
+    elsif event_details[:input_category] == "Drinks"
       filepath = "app/services/test_results/bar_coctail|pub|bar.json"
-    elsif event[:input_category] == "Activity"
+    elsif event_details[:input_category] == "Activity"
       filepath = "app/services/test_results/tourist_attraction_activity|adventure|experience|interactive.json"
-    elsif event[:input_category] == "Outdoors"
+    elsif event_details[:input_category] == "Outdoors"
       filepath = "app/services/test_results/tourist_attraction_park|walk|outdoor|outside|nature.json"
-    elsif event[:input_category] == "Attraction"
+    elsif event_details[:input_category] == "Attraction"
       filepath = "app/services/test_results/tourist_attraction_tourist|attraction.json"
-    elsif event[:input_category] == "history_art_culture"
+    elsif event_details[:input_category] == "history_art_culture"
       filepath = "app/services/test_results/x_museum_history|culture|immersive|art|culture|exhibition.json"
-    elsif event[:input_category] == "dining_dinner_drinks"
+    elsif event_details[:input_category] == "dining_dinner_drinks"
       filepath = "app/services/test_results/x_restaurant_steak|coctail|wine|beer_maxprice3.json"
-    elsif event[:input_category] == "dining_lunch_drinks"
+    elsif event_details[:input_category] == "dining_lunch_drinks"
       filepath = "app/services/test_results/x_restaurant_steak|coctail|wine|beer_maxprice3.json"
-    elsif event[:input_category] == "activity_attraction"
+    elsif event_details[:input_category] == "activity_attraction"
       filepath = "app/services/test_results/tourist_attraction_activity|adventure|experience|interactive.json"
     end
 
     serialized_places = File.read(filepath)
     JSON.parse(serialized_places)
+  end
 
+  def populate_place(place)
+    {
+      name: place["name"], # String
+      types: place["types"], # Array
+      rating: place["rating"].to_i, # Integer
+      user_ratings_total: place["user_ratings_total"].to_i, # Integer
+      photo_reference: place["photos"][0]["photo_reference"], # String
+      location: "#{place["geometry"]["location"]["lat"]},#{place["geometry"]["location"]["lng"]}", # String
+      place_id: place["place_id"], # String
+      price_level: place["price_level"] # Integer
+    }
   end
 end
