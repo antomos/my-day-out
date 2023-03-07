9586ed199c6d7e76bedb1e476a38ce4f121daec2
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-03-07 16:29:48 +0000

    Outputs basic info in event show as test. Calls API or test JSONs for details. Assembles itinerary

--- app/services/populate_itinerary.rb
+++ app/services/populate_itinerary.rb
@@ -36,60 +36,58 @@ class PopulateItinerary < ApplicationRecord
                                 }).perform
 
 
-      raise
-
     end
 
   end
 
   def generate_url(search_location, event_details)
-    key = 1234
+    # 1234"
 
     URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{search_location}&radius=#{event_details[:radius]}&type=#{event_details[:place_type]}&keyword=#{event_details[:keyword]}&maxprice=#{event_details[:maxprice]}&rankby=prominence&key=#{key}")
   end
 
   def fetch_places(url, event_details) # DELETE EVENT PARAM
-    # https = Net::HTTP.new(url.host, url.port)
-    # https.use_ssl = true
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
 
-    # request = Net::HTTP::Get.new(url)
+    request = Net::HTTP::Get.new(url)
 
-    # response = https.request(request)
+    response = https.request(request)
 
-    # response_json = response.read_body
-    # JSON.parse(response_json)
+    response_json = response.read_body
+    JSON.parse(response_json)
 
-    # TEST JSONS
-    if event_details[:input_category] == "History"
-      filepath = "app/services/test_results/museum_history|culture|immersive.json"
-    elsif event_details[:input_category] == "Art & Culture"
-      filepath = "app/services/test_results/art_gallery_art|culture|exhibition.json"
-    elsif event_details[:input_category] == "Shopping"
-      filepath = "app/services/test_results/establishment_ shopping|mall|centre|department|store.json"
-    elsif event_details[:input_category] == "dining_dinner"
-      filepath = "app/services/test_results/restaurant_japanese|ramen_minprice-_maxprice-.json"
-    elsif event_details[:input_category] == "dining_lunch"
-      filepath = "app/services/test_results/restaurant_japanese|ramen_minprice-_maxprice-.json"
-    elsif event_details[:input_category] == "Drinks"
-      filepath = "app/services/test_results/bar_coctail|pub|bar.json"
-    elsif event_details[:input_category] == "Activity"
-      filepath = "app/services/test_results/tourist_attraction_activity|adventure|experience|interactive.json"
-    elsif event_details[:input_category] == "Outdoors"
-      filepath = "app/services/test_results/tourist_attraction_park|walk|outdoor|outside|nature.json"
-    elsif event_details[:input_category] == "Attraction"
-      filepath = "app/services/test_results/tourist_attraction_tourist|attraction.json"
-    elsif event_details[:input_category] == "history_art_culture"
-      filepath = "app/services/test_results/x_museum_history|culture|immersive|art|culture|exhibition.json"
-    elsif event_details[:input_category] == "dining_dinner_drinks"
-      filepath = "app/services/test_results/x_restaurant_steak|coctail|wine|beer_maxprice3.json"
-    elsif event_details[:input_category] == "dining_lunch_drinks"
-      filepath = "app/services/test_results/x_restaurant_steak|coctail|wine|beer_maxprice3.json"
-    elsif event_details[:input_category] == "activity_attraction"
-      filepath = "app/services/test_results/tourist_attraction_activity|adventure|experience|interactive.json"
-    end
+    # # TEST JSONS
+    # if event_details[:input_category] == "History"
+    #   filepath = "app/services/test_results/museum_history|culture|immersive.json"
+    # elsif event_details[:input_category] == "Art & Culture"
+    #   filepath = "app/services/test_results/art_gallery_art|culture|exhibition.json"
+    # elsif event_details[:input_category] == "Shopping"
+    #   filepath = "app/services/test_results/establishment_ shopping|mall|centre|department|store.json"
+    # elsif event_details[:input_category] == "dining_dinner"
+    #   filepath = "app/services/test_results/restaurant_japanese|ramen_minprice-_maxprice-.json"
+    # elsif event_details[:input_category] == "dining_lunch"
+    #   filepath = "app/services/test_results/restaurant_japanese|ramen_minprice-_maxprice-.json"
+    # elsif event_details[:input_category] == "Drinks"
+    #   filepath = "app/services/test_results/bar_coctail|pub|bar.json"
+    # elsif event_details[:input_category] == "Activity"
+    #   filepath = "app/services/test_results/tourist_attraction_activity|adventure|experience|interactive.json"
+    # elsif event_details[:input_category] == "Outdoors"
+    #   filepath = "app/services/test_results/tourist_attraction_park|walk|outdoor|outside|nature.json"
+    # elsif event_details[:input_category] == "Attraction"
+    #   filepath = "app/services/test_results/tourist_attraction_tourist|attraction.json"
+    # elsif event_details[:input_category] == "history_art_culture"
+    #   filepath = "app/services/test_results/x_museum_history|culture|immersive|art|culture|exhibition.json"
+    # elsif event_details[:input_category] == "dining_dinner_drinks"
+    #   filepath = "app/services/test_results/x_restaurant_steak|coctail|wine|beer_maxprice3.json"
+    # elsif event_details[:input_category] == "dining_lunch_drinks"
+    #   filepath = "app/services/test_results/x_restaurant_steak|coctail|wine|beer_maxprice3.json"
+    # elsif event_details[:input_category] == "activity_attraction"
+    #   filepath = "app/services/test_results/tourist_attraction_activity|adventure|experience|interactive.json"
+    # end
 
-    serialized_places = File.read(filepath)
-    JSON.parse(serialized_places)
+    # serialized_places = File.read(filepath)
+    # JSON.parse(serialized_places)
   end
 
   def populate_place(place)
