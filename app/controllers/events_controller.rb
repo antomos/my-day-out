de4fa981f457035bc695bfb65a60e33bad3b6164
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-03-10 13:25:39 +0000

    Fixes issue with saving duplicate new place objects to the places table when updating event place.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,11 +28,11 @@ class EventsController < ApplicationController
     # binding.pry
 
     if Place.find_by(search_place_details_id: @search_place_details[:place_id])
-      place = Place.find_by(search_place_details_id: @search_place_details[:place_id])
+      new_place = Place.find_by(search_place_details_id: @search_place_details[:place_id])
     else
       place_details = RequestPlaceDetail.new(@search_place_details).perform
 
-      place = PopulatePlace.new({
+      new_place = PopulatePlace.new({
                                 # event_details: @event_details,
                                   search_place_details: @search_place_details,
                                   place_details: place_details
@@ -42,7 +42,7 @@ class EventsController < ApplicationController
     # DOUBE ENTRY TO DB MADE BEFORE THIS LINE!!!
     # ISSUE WHEN SAVING - CHANGE THE EXISTING PLACE CHECK TO THE ONE LINE FIND_BY_OR_CREATE??
 
-    @event.update(place: place)
+    @event.update(place: new_place)
     # REFRESH PAGE AUTOMATICALLY
   end
 
