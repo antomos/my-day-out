747d82e65bd8aa422f3434dc6c8fef715d3231f2
Giulio de Cadilhac <g.decadilhac@gmail.com>
2023-03-16 17:01:56 +0000

    calling CheckOpenEvents 4 times in two controllers, removed wrong migrations

--- db/migrate/20230316123116_create_check_open_events_table.rb
+++
@@ -1,7 +0,0 @@
-class CreateCheckOpenEventsTable < ActiveRecord::Migration[7.0]
-  def change
-    create_table :check_open_events_tables do |t|
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20230316124046_drop_check_open_events_table.rb
+++
@@ -1,5 +0,0 @@
-class DropCheckOpenEventsTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :check_open_events_tables
-  end
-end
