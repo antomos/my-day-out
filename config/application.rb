dfcec2bec484910519dacb0b60f5040e84a519ff
antonio moschetti <antoniomoschetti@hotmail.it>
2023-03-18 16:15:31 +0000

    domain issues

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module MyDayOut
   class Application < Rails::Application
     config.generators do |generate|
@@ -15,6 +16,7 @@ module MyDayOut
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    Rails.application.routes.default_url_options[:protocol]= 'https'
 
     # Configuration for the application, engines, and railties goes here.
     #
