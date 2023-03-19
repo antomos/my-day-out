bda686ed5e02932128856b70ab2e1d108c781fad
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-03-19 14:07:26 +0000

    Prepare to Post to Controller

--- app/services/create_user_event.rb
+++ app/services/create_user_event.rb
@@ -20,10 +20,39 @@ class CreateUserEvent < ApplicationRecord
     @new_event.order_number = order_number.to_s
     @new_event.event_duration = 60
 
-    if Place.find_by(name: @place)
+    if Place.find_by(name: @place.split(",").first)
+      place = Place.find_by(name: @place.split(",").first)
+    else
+      place_details = fetch_place_details
+      create_place(place_details)
+    end
 
-    @new_event.place_id = 41
-
+    @new_event.place = place
     @new_event
+  end
+
+  def fetch_place_details
+
+    # ISSUE - PLACE SEARCH ONLY RETURNS THE GOD DAMN PLACE ID! DOUBLE API REQUESTS!!!
+    # key = ENV["GOOGLE_API_KEY"]
+    # URI("https://maps.googleapis.com/maps/api/place/details/json?place_id=#{@place_details[:place_id]}&key=#{key}")
+    # "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=#{Place}&inputtype=textquery&key=#{key}"
+
+
+    ######################### API JSONS ##########################
+    # https = Net::HTTP.new(url.host, url.port)
+    # https.use_ssl = true
+    # request = Net::HTTP::Get.new(url)
+    # response = https.request(request)
+    # response_json = response.read_body
+    # JSON.parse(response_json)
+    ##############################################################
+
+
+
+  end
+
+  def create_place
+
   end
 end
