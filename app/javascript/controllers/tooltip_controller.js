536f7d86ee5dca4c5a87a7ada57e815c1e763256
antonio moschetti <antoniomoschetti@hotmail.it>
2023-03-21 21:04:03 +0000

    popup and cards fixed

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,7 +1,8 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import { Tooltip } from "bootstrap/dist/js/bootstrap.bundle.js";
 
-// Connects to data-controller="tooltip"
 export default class extends Controller {
   connect() {
+    new Tooltip(this.element)
   }
 }
